ca949aaa5f395a0fbb0232aec07d0a88d657dea5
Isai-In <alejandroisaiin@gmail.com>
2017-07-05 08:41:15 -0500

    CreateAccountActivity added

--- app/src/main/java/com/isaiin/scrappify/LoginActivity.java
+++ app/src/main/java/com/isaiin/scrappify/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.isaiin.scrappify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.isaiin.scrappify.Views.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent i = new Intent(this, CreateAccountActivity.class);
+        startActivity(i);
     }
 }
